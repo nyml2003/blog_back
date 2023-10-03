6de38d39e8fb5f8b3519bf74569489210e56ba8d
nyml2003 <2492181618@qq.com>
2023-10-04 02:01:21 +0800

    修改了博文修改页面

--- gui/src/utils/normalLogic.js
+++ gui/src/utils/normalLogic.js
@@ -3,8 +3,5 @@ export function checkSame(data,dataCopy){
   return JSON.stringify(data) === JSON.stringify(dataCopy)
 }
 export function isNumber(value){
-  console.log('isNumber: ', value)
-  console.log(typeof value)
-  console.log(!isNaN(value))
   return typeof value === 'number' && !isNaN(value)
 }
